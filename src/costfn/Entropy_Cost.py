354f8d5ea02743676218c0dfce9ed8a1c89287a7
RomanChiva <rchivag@gmail.com>
2024-04-22 16:31:19 +0200

    Initial Commit

---
+++ src/costfn/Entropy_Cost.py
@@ -0,0 +1,216 @@
+#!/usr/bin/python3
+
+import numpy as np
+from scipy.interpolate import CubicSpline
+from utils.cubicspline import CubicSpline2D
+import torch
+from sklearn.mixture import GaussianMixture
+import time
+from utils.plotter import LiveBarChart, MPPIVisualization
+from Experiments.msg import Obstacle, ObstacleArray
+
+
+class ObjectiveLegibility(object):
+
+    def __init__(self, cfg, obstacles, interface):
+        # Create two possible goals used in pred_model
+        self.cfg = cfg
+        self.goal_index = cfg.costfn.goal_index
+        self.other_goal = 1 - self.goal_index
+        self.goals = torch.tensor(self.cfg.costfn.goals, dtype=torch.float32, device=self.cfg.mppi.device)
+        self.v_ref = torch.tensor(cfg.v_ref, device=cfg.mppi.device)
+        self.x_ref = np.array([0, 3, 6, 9, 12, 15, 18, 21, 30, 34, 38])
+        self.y_ref = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.reference_spline = CubicSpline2D(self.x_ref, self.y_ref)
+        self.obstacles = obstacles
+        self.interface = interface
+        self.plotter = LiveBarChart(['A', 'B'])
+        self.plotter2 = MPPIVisualization()
+
+    
+
+    # Cost Function for free navigation (Straight to goal)
+    def compute_cost(self, state, u, t, obst):
+        ## The state coming in is a tensor with all the different trajectory samples
+        ## Goal Cost
+
+        self.plotter2.animate(state[:,:,0:2])
+
+        goal_cost = self.goal_cost(state)
+        # Legibility Cost
+
+        entropy_cost = self.entropy_cost(state)
+        entropy_cost = entropy_cost.reshape(-1)
+
+        # Obstacle Cost
+        obstacle_cost = self.obstacle_cost(state)
+        obstacle_cost = obstacle_cost.reshape(-1)
+
+        # Add them
+        return goal_cost + 10*entropy_cost + obstacle_cost
+     
+    def alpha(self):
+
+        grad = self.cfg.costfn.alpha_max/self.cfg.costfn.alpha_steps
+
+        if self.interface.timesteps < self.cfg.costfn.alpha_steps:
+            return self.interface.timesteps * grad
+        else:
+            return self.cfg.costfn.alpha_max
+
+    def goal_cost(self, state):
+        
+
+        state_goal = state.permute(1, 0, 2)
+        # Now reshape to (T*K, nx)
+        state_goal = state_goal.reshape(-1, self.cfg.nx)
+        pos_goal = state_goal[:, 0:2]
+        goal_cost = torch.linalg.norm(pos_goal - self.goals[self.goal_index], axis=1)
+
+        return goal_cost
+
+
+
+    def entropy_cost(self, state):
+
+        ## Get Predictions
+        state = state.permute(1, 0, 2)
+
+        weights = self.pred_mod_goals(state)
+        
+        ## Compute entropy of the weights at each timestep and sample along the 2nd dimesnion
+        entropy = -torch.sum(weights * torch.log2(weights), dim=2)
+        
+        return entropy
+
+    def pred_mod_goals(self, state):
+
+
+        pos = state[:, :, 0:2]
+        # Find vectors to goals 1 and 2 (Goal[-] shape[1,2] pos shape [T, K, 2])
+        vector_goal1 = self.goals[0] - pos
+        vector_goal2 = self.goals[1] - pos
+
+        # Find magnitudes of vectors
+        magnitude_goal1 = torch.linalg.norm(vector_goal1, axis=2)
+        magnitude_goal2 = torch.linalg.norm(vector_goal2, axis=2)
+
+        # Find distance travelled so far
+        distance = self.compute_path_length(self.interface.trajectory)
+        
+
+        path_lengths = self.compute_path_lengths(pos)
+        
+        ## Add length of path so far to path_lengths
+        path_lengths = path_lengths + distance
+
+    
+        # FInd magnitude of goals from [0,0]
+        V_g1_0 = torch.linalg.norm(self.goals[0])
+        V_g2_0 = torch.linalg.norm(self.goals[1])
+     
+
+        # Compute weights
+        weight_1 = torch.exp(V_g1_0 - path_lengths - magnitude_goal1)
+        weight_2 = torch.exp(V_g2_0 - path_lengths - magnitude_goal2)
+        
+
+        # Normalize the weights
+        weights = torch.stack([weight_1, weight_2], dim=2)
+        weights = weights / weights.sum(dim=2).unsqueeze(-1)
+        
+
+        return weights
+    
+
+
+
+    def compute_path_length(self, path):
+        path_diff = np.diff(path, axis=0)
+        path_length = np.linalg.norm(path_diff, axis=1).sum()
+        return path_length
+    
+
+    def compute_path_lengths(self, paths):
+
+
+        # Get position XY and make it tensor
+        position =  torch.tensor([self.interface.odom_msg.pose.pose.position.x,
+                                self.interface.odom_msg.pose.pose.position.y], device=self.cfg.mppi.device)
+        # Subtract the current position from the rest of the path
+        paths = paths - position.unsqueeze(0).unsqueeze(0)
+
+        # append row of zeros to the beginning of the path
+        paths = torch.cat([torch.zeros_like(paths[0:1]), paths], dim=0)
+
+        # Compute the differences between consecutive points in each path
+        path_diffs = torch.diff(paths, dim=0)
+        
+        # Compute the length of each segment in each path
+        segment_lengths = torch.norm(path_diffs, dim=-1)
+
+        # Compute the cumulative sum of segment lengths along the timesteps dimension
+        path_lengths = torch.cumsum(segment_lengths, dim=0)
+
+        return path_lengths
+    
+
+    def obstacle_cost(self, state):
+        
+
+        pos = state[:, :, 0:2]
+        horizon = pos.shape[1]
+        # Propagate the positions of the obstacles K timesteps into the future
+        obstacles = self.interface.obstacles
+        future_positions = self.propagate_positions(obstacles, horizon)
+
+        # Compute the distances between the future positions of the obstacles and the trajectory samples
+        distances = self.compute_distances(pos, future_positions)
+        min_distances, _ = distances.min(dim=2)
+        
+        # Check if the future positions of the obstacles are within the robot's safety radius
+        safety_radius = self.cfg.costfn.safety_radius
+        collision_cost = (min_distances < safety_radius).float()*self.cfg.costfn.collision_cost
+
+        # Sum along samples dimension
+        collision_cost_sum = collision_cost.sum(dim=1)
+        print(collision_cost_sum.shape)
+        # Print collision cost sum minimum
+        print(collision_cost_sum.min())
+
+        return collision_cost
+
+
+
+    def compute_distances(self, samples, obstacles):
+
+        # Add new axes to match the shapes
+        samples = samples.unsqueeze(2)
+        obstacles = obstacles.unsqueeze(0)
+
+        # Compute the distances using broadcasting
+        distances = torch.sqrt(((samples - obstacles)**2).sum(-1))
+        # Sum along the samples direction to get the obstacle cost per sample
+
+        return distances
+        
+        
+
+    def propagate_positions(self, obstacle_array, K):
+        # Convert positions and velocities to PyTorch tensors
+        positions = torch.tensor([[
+            obstacle.position.position.x,
+            obstacle.position.position.y
+        ] for obstacle in obstacle_array.obstacles])
+
+        velocities = torch.tensor([[
+            obstacle.velocity.linear.x,
+            obstacle.velocity.linear.y
+        ] for obstacle in obstacle_array.obstacles])
+
+        # Create a new axis for the timesteps and use broadcasting to propagate the positions
+        timesteps = torch.arange(K)[:, None, None]
+        future_positions = positions + velocities * timesteps
+
+        return future_positions
+
---
+++ src/obstacles/moving_obstacles.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+import rospy
+from geometry_msgs.msg import Pose, Twist
+from Experiments.msg import Obstacle, ObstacleArray
+from visualization_msgs.msg import Marker, MarkerArray
+import numpy as np
+import yaml
+import os
+
+class MovingObstacle:
+    def __init__(self, initial_position, velocity, moving_range):
+        self.position = np.array(initial_position, dtype=np.float32)
+        self.speed = float(velocity)
+        self.velocity = np.array([self.speed, 0], dtype=np.float32)
+        self.moving_range = np.array(moving_range, dtype=np.float32)
+        self.target_position = moving_range[1]
+
+    def update_position(self, timestep):
+        # Calculate the direction vector
+        direction = self.target_position - self.position
+
+        # Normalize the direction vector
+        direction /= np.linalg.norm(direction)
+
+        # Calculate the displacement
+        self.velocity = direction * self.speed * timestep
+
+        # Add a small random noise to the displacement
+        noise = np.random.normal(scale=0.01, size=self.velocity.shape)
+        #self.velocity += noise
+
+        # Update the position
+        self.position += self.velocity
+
+        # If the obstacle has reached the target position, set the target position to the other end of the moving range
+        if np.linalg.norm(self.target_position - self.position) < self.speed * timestep:
+            self.target_position = self.moving_range[1] if np.array_equal(self.target_position, self.moving_range[0]) else self.moving_range[0]
+
+
+
+
+
+
+class MovingObstaclesNode:
+    def __init__(self, path):
+
+        with open(path, 'r') as file:
+            config = yaml.safe_load(file)
+
+        self.obstacles = [MovingObstacle(**params) for params in config['obstacles']]
+        self.pub = rospy.Publisher('obstacle_states', ObstacleArray, queue_size=10)
+        self.marker_pub = rospy.Publisher('obstacle_markers', MarkerArray, queue_size=10)
+
+    def update_and_publish(self, timestep):
+
+        obstacle_array = ObstacleArray()
+        for obstacle in self.obstacles:
+            obstacle.update_position(timestep)
+            obstacle_msg = Obstacle()
+
+            obstacle_msg.position = Pose()
+            obstacle_msg.position.position.x = obstacle.position[0]
+            obstacle_msg.position.position.y = obstacle.position[1]
+            obstacle_msg.position.position.z = 0.2
+
+            obstacle_msg.velocity = Twist()
+            obstacle_msg.velocity.linear.x = obstacle.velocity[0]
+            obstacle_msg.velocity.linear.y = obstacle.velocity[1]
+            obstacle_msg.velocity.linear.z = 0
+
+            obstacle_array.obstacles.append(obstacle_msg)
+        self.pub.publish(obstacle_array)
+
+        # Publish Marker Array for RVIZ
+
+        marker_array = MarkerArray()
+        for i, obstacle in enumerate(self.obstacles):
+
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.scale.x = 1.0
+            marker.scale.y = 1.0
+            marker.scale.z = 1.0
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 0.9
+            marker.color.b = 0.0
+            marker.pose.position.x = obstacle.position[0]
+            marker.pose.position.y = obstacle.position[1]
+            marker.pose.position.z = 0.2
+            marker.id = i
+            marker_array.markers.append(marker)
+
+        self.marker_pub.publish(marker_array)
+
+
+
+
+if __name__ == '__main__':
+    
+    rospy.init_node('moving_obstacles_node')
+
+    # Load Path
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    config_dir = os.path.join(current_dir, '..', '..', 'conf', 'obstacles')
+    path = os.path.join(config_dir, 'MovingPointObstacles.yaml')
+
+    
+    node = MovingObstaclesNode(path)
+    timestep = 0.1
+    rate = rospy.Rate(10) # 10 Hz
+
+
+    while not rospy.is_shutdown():
+        node.update_and_publish(timestep)
+        rate.sleep()
